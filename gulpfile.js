73da48efffa791cecaa15851069310ab5ff8d67b
Harrison Goldstein <hgoldstein95@gmail.com>
2017-10-18 19:43:43 -0400

    Updated config

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function() {
+gulp.task("ts", function() {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["ts"], function() {
 });
